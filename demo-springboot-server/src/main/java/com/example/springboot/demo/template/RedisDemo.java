97089201c98a1cd52c3910eef61c752b6d595645
zhanghesheng <15939630623@163.com>
2018-05-02 09:53:29 +0800

    redis 新加方法案例

--- demo-springboot-server/src/main/java/com/example/springboot/demo/template/RedisDemo.java
+++ demo-springboot-server/src/main/java/com/example/springboot/demo/template/RedisDemo.java
@@ -13,14 +13,16 @@ import redis.clients.jedis.JedisPool;
 public class RedisDemo {
 
     /**
-     * 一定要在微服务启动入口DemoMicroService中引入common工程中的RedisConfiguration配置类
+     * 须引入common工程中的RedisConfiguration配置类
      */
     @Autowired
     private JedisPool jedisPool;
     @Autowired
     private JedisCluster jedisCluster;
 
-    //string 存储
+     /**
+     * string 存储
+     */
     public String setStringRedis(String key, String value) {
         JedisCommands commands = JedisUtils.getJedisCommands(jedisCluster, jedisPool);
         String s = null;
@@ -42,8 +44,9 @@ public class RedisDemo {
         return s;
     }
 
-
-    //string 取值
+    /**
+     * string 取值
+     */
     public String getStringRedis(String key) {
         JedisCommands commands = JedisUtils.getJedisCommands(jedisCluster, jedisPool);
         String s = null;
@@ -56,4 +59,36 @@ public class RedisDemo {
         }
         return s;
     }
+
+
+    /**
+     * string 存储
+     * @param key   键
+     * @param value 值
+     * @param nxxx  只能是"NX"或"XX"
+     *              如果取NX，则只有当key不存在是才进行set，如果取XX，则只有当key已经存在时才进行set
+
+     * @param expx  只能是"EX"或"PX"
+     *              代表数据过期时间的单位，EX代表秒，PX代表毫秒。
+     * @param time  有效期时间
+     */
+    public String setStringRedis(String key, String value,String nxxx,String expx,long time) {
+        JedisCommands commands = JedisUtils.getJedisCommands(jedisCluster, jedisPool);
+        String s = null;
+        try {
+            /**
+             * 往redis中放入字符串值
+             */
+            s =  commands.set(key, value, nxxx, expx, time);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            /**
+             * 一定要关闭redis资源
+             */
+            JedisUtils.closeJedis(commands);
+        }
+        return s;
+    }
+
 }
